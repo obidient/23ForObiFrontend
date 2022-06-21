96893cb7f1ff41604030aee76bb135c2dfd70514
Odizee <odizeeplatform@gmail.com>
2022-06-21 03:16:14 -1100

    added top 5 contributors image

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -3,6 +3,13 @@ import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 import StateProgress from '../misc/StateProgress';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+import village_img_1 from '../../assets/village_img_1.png';
+import village_img_2 from '../../assets/village_img_2.png';
+import village_img_3 from '../../assets/village_img_3.png';
+import village_img_4 from '../../assets/village_img_4.png';
+import village_img_5 from '../../assets/village_img_5.png';
 
 const Card = ({ state, progress, voteControl, type, village }) => {
   const stateSlug = state?.split(' ').slice(0, -1).join(' ').toLowerCase();
@@ -39,7 +46,13 @@ const Card = ({ state, progress, voteControl, type, village }) => {
           <p>{voteControl}%</p>
         </div>
       ) : (
-        <div></div>
+        <div className={styles.contributors_img}>
+          <Image src={village_img_1} />
+          <Image src={village_img_2} />
+          <Image src={village_img_3} />
+          <Image src={village_img_4} />
+          <Image src={village_img_5} />
+        </div>
       )}
     </div>
   );
