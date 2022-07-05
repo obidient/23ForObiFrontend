0ef561bf7f268921676569f8a02baad4aa190481
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-07-05 20:46:53 +0100

    ft--fetchApi, missing slug added to stateDetails

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = ''
+export const baseUrl = 'https://api.23forobi.com'
 
 export const fetchApi = async (url) => {
     const {data} = await axios.get(url)
