a9769a4cebee29ac8f64b917c7030b59f19a2067
chiziivictor <chizivwokoma@gmail.com>
2022-07-06 02:25:18 +0100

    modify form controls on dashboard page

--- components/Dashboard/DashboardMain.js
+++ components/Dashboard/DashboardMain.js
@@ -9,6 +9,13 @@ import SelectInput from './../misc/SelectInput';
 import VillageContext from '../../Context/villageContext';
 import useVillage from '../../Context/villageContext';
 
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import dynamic from 'next/dynamic';
+const FormikControl = dynamic(() => import('../Forms/FormikControl'), {
+  ssr: false,
+});
+
 const DashboardMain = () => {
   ////// Modal State
   const [showModal, setShowModal] = useState(false);
@@ -56,6 +63,44 @@ const DashboardMain = () => {
 
   const { villages, removeVillage } = useVillage();
 
+  //Initialize select options
+  const stateOptions = [
+    {
+      label: 'Rivers',
+      value: 'Rivers State',
+    },
+    {
+      label: 'Lagos',
+      value: 'Lagos State',
+    },
+    {
+      label: 'Ogun',
+      value: 'Ogun State',
+    },
+  ];
+  const villageOptions = [
+    {
+      label: 'Ezeani Village 1',
+      value: 'Ezeani Village',
+    },
+    {
+      label: 'Osusus Village 2',
+      value: 'Osusu Village',
+    },
+    {
+      label: 'Ariara Village 3',
+      value: 'Ariara Village',
+    },
+  ];
+
+  // Form validation schema using Yup
+  const contributorValidationSchema = Yup.object({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    state: Yup.string().required('Required'),
+    village: Yup.string().required('Required'),
+  });
+
   //Effect to hide scroll
   useEffect(() => {
     const body = document.querySelector('body');
@@ -71,7 +116,25 @@ const DashboardMain = () => {
           <h3>VIllages you have control in</h3>
           <p>Add the villages you have control over within your state.</p>
           <div className={styles.input}>
-            <SelectInput default="Select your village" />
+            <Formik
+              initialValues={{ village: '' }}
+              validationSchema={Yup.object({
+                village: Yup.string().required('Required'),
+              })}
+              onSubmit={(values) => console.log('Form data', values)}
+            >
+              {({ values }) => (
+                <Form>
+                  <FormikControl
+                    values={values}
+                    control="select"
+                    placeholder="Select your village"
+                    name="village"
+                    options={villageOptions}
+                  />
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
         <div className={styles.village_control__outputs}>
@@ -93,18 +156,53 @@ const DashboardMain = () => {
       <hr />
       <div className={styles.main_bottom}>
         <div className={styles.main_bottom__contact}>
-          <form action="">
-            <h5>Specify your key contact</h5>
-            <p>
-              Specify your key contact information we can always reach you on.
-            </p>
-            <div className={styles.input_field}>
-              <input type="text" placeholder="Full name" />
-              <input type="text" placeholder="Phone number" />
-              <input type="text" placeholder="Whatsapp number" />
-            </div>
-            <button className="btn_dark">Save</button>
-          </form>
+          <Formik
+            initialValues={{ name: '', phone: '', whatsapp: '' }}
+            validationSchema={Yup.object({
+              name: Yup.string().required('Required'),
+              phone: Yup.number()
+                .required('Required')
+                .typeError('Invalid phone number'),
+              whatsapp: Yup.number()
+                .required('Required')
+                .typeError('Invalid phone number'),
+            })}
+            onSubmit={(values) => console.log('Form data', values)}
+          >
+            {({ values }) => (
+              <Form>
+                <h5>Specify your key contact</h5>
+                <p>
+                  Specify your key contact information we can always reach you
+                  on.
+                </p>
+                &nbsp;
+                <div className={styles.input_field}>
+                  <FormikControl
+                    values={values}
+                    control="input"
+                    placeholder="Full name"
+                    name="name"
+                  />
+                  <FormikControl
+                    values={values}
+                    control="input"
+                    placeholder="Phone number"
+                    name="phone"
+                  />
+                  <FormikControl
+                    values={values}
+                    control="input"
+                    placeholder="Whatsapp number"
+                    name="whatsapp"
+                  />
+                </div>
+                <button className="btn_dark" type="submit">
+                  Login
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
         <div className={styles.main_bottom__contributors}>
           <div className={styles.heading}>
@@ -125,36 +223,55 @@ const DashboardMain = () => {
                 <div className={styles.modal__body}>
                   <p>Kindly enter the details of a contributor</p>
                   <div className={styles.details_form}>
-                    <form action="">
-                      <input
-                        type="text"
-                        name="first_name"
-                        placeholder="First name"
-                        value={contributor.first_name}
-                        onChange={contributeChange}
-                      />
-                      <input
-                        type="text"
-                        name="last_name"
-                        placeholder="Last Name"
-                        value={contributor.last_name}
-                        onChange={contributeChange}
-                      />
-                      <div className={styles.select}>
-                        <SelectInput option="Phone Number" name="number" />
-                      </div>
-                      <div className={styles.select}>
-                        <SelectInput option="Select a village" name="village" />
-                      </div>
-                      <div className={`${styles.btn_submit}`}>
-                        <input
-                          type="button"
-                          value="Continue"
-                          onClick={contributeSubmit}
-                          className="btn_dark"
-                        />
-                      </div>
-                    </form>
+                    <Formik
+                      initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        state: '',
+                        village: '',
+                      }}
+                      validationSchema={contributorValidationSchema}
+                      onSubmit={(values) => console.log('Form data', values)}
+                    >
+                      {({ values }) => (
+                        <Form autoComplete="off">
+                          <FormikControl
+                            values={values}
+                            control="input"
+                            placeholder="First Name"
+                            name="firstName"
+                            type="text"
+                          />
+                          <FormikControl
+                            values={values}
+                            control="input"
+                            placeholder="Last Name"
+                            name="lastName"
+                            type="text"
+                          />
+                          <FormikControl
+                            values={values}
+                            control="select"
+                            placeholder="Select your state"
+                            name="state"
+                            options={stateOptions}
+                          />
+                          <FormikControl
+                            values={values}
+                            control="select"
+                            placeholder="Select your village"
+                            name="village"
+                            options={villageOptions}
+                          />
+                          <button
+                            type="submit"
+                            className="btn_dark w-full rounded-full h-20 mt-9"
+                          >
+                            Continue
+                          </button>
+                        </Form>
+                      )}
+                    </Formik>
                   </div>
                 </div>
               </div>
