65eaae3b30dafa29bedfd72e62ea72d9e8b207b6
Odizee <odizeeplatform@gmail.com>
2022-07-09 03:40:24 -1100

    made deliver modal a component

---
+++ components/Modal/DeliverModal.js
@@ -0,0 +1,163 @@
+import styles from './Styles.module.scss';
+import React, { useEffect, useRef, useCallback } from 'react';
+import Modal from './Index';
+import Image from 'next/image';
+
+import { MdOutlineArrowBackIosNew } from 'react-icons/md';
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
+
+import img1 from '../../assets/how_it_works_img1.png';
+import img2 from '../../assets/how_it_works_img2.png';
+import img3 from '../../assets/how_it_works_img3.png';
+import { useState } from 'react';
+import Link from 'next/link';
+
+const DeliverModal = ({ show, onClose }) => {
+  //Carousel
+  const slides = [
+    {
+      text: {
+        heading: 'Create an account',
+        span: 'by clicking on deliver votes',
+        body: 'Create an account in a few easy steps by using your favourite social media account.',
+      },
+      image: img1,
+    },
+    {
+      text: {
+        heading: 'Provide your',
+        span: 'details',
+        body: 'Provide your details in few simple steps, all you have to do is login with your social media account.',
+      },
+      image: img2,
+    },
+    {
+      text: {
+        heading: 'Answer a',
+        span: 'few questions',
+        body: 'Provide a few answers about your PVC status in order to get the neccessary information needed.',
+      },
+      image: img3,
+    },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slideInterval = useRef();
+
+  const prev = () => {
+    // startSlideTimer();
+
+    const index = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
+    setCurrentSlide(index);
+  };
+
+  const next = () => {
+    // startSlideTimer();
+
+    const index = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
+    setCurrentSlide(index);
+  };
+
+  const switchIndex = (index) => {
+    // startSlideTimer();
+    setCurrentSlide(index);
+  };
+  return (
+    <div className={styles.backdrop}>
+      <Modal show={show} onClose={onClose} width="79.4rem">
+        <div className={styles.deliver_modal}>
+          <div className={`${styles.deliver_modal__heading} modal_heading`}>
+            <h2>
+              Learn more <br />
+              <span>About the campaign</span>
+            </h2>
+            <button className={`closeBtn`} onClick={onClose}>
+              &times;
+            </button>
+          </div>
+          <div className={styles.deliver_modal__body}>
+            <div className={styles.carousel}>
+              <div
+                className={styles.carousel__inner}
+                style={{
+                  transform: `translate(${-currentSlide * 100}%)`,
+                }}
+              >
+                {slides.map((slide, index) => {
+                  return (
+                    <div className={styles.carousel__item}>
+                      <div className={styles.details}>
+                        <div className={styles.text}>
+                          <h3>
+                            {slide.text.heading} <br />
+                            <span>{slide.text.span}</span>
+                          </h3>
+                          <p>{slide.text.body}</p>
+                        </div>
+                        <div className={styles.img}>
+                          <Image src={slide.image} />
+                        </div>
+                        <div className={styles.text}>
+                          <h3>
+                            {slides[index + 1]?.text.heading}
+                            <br />
+                            <span>{slides[index + 1]?.text.span}</span>
+                          </h3>
+                          <p>{slides[index + 1]?.text.body}</p>
+                        </div>
+                        <div className={styles.img}>
+                          <Image src={slides[1].image} />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div
+                className={`${styles.carousel__control} ${
+                  currentSlide === 0 ? styles.slideNone : styles.left
+                }`}
+                onClick={prev}
+              >
+                <MdOutlineArrowBackIosNew />
+              </div>
+              <div
+                className={`${styles.carousel__control} ${
+                  currentSlide === slides.length - 1
+                    ? styles.slideNone
+                    : styles.right
+                }`}
+                onClick={next}
+              >
+                <MdOutlineArrowForwardIos />
+              </div>
+            </div>
+            <div className={styles.second_layer}>
+              <div className={styles.carousel_indicators}>
+                {slides.map((_, index) => (
+                  <button
+                    className={`${styles.carousel_indicator_item} ${
+                      currentSlide === index ? styles.active : ''
+                    }`}
+                    onClick={() => switchIndex(index)}
+                  ></button>
+                ))}
+              </div>
+              <Link href="/how-it-works">Learn More</Link>
+            </div>
+            <button
+              type="submit"
+              className={`btn_dark rounded-full ${styles.btn}`}
+            >
+              Create an account
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default DeliverModal;
--- components/NavBar/Index.js
+++ components/NavBar/Index.js
@@ -13,6 +13,7 @@ import { MdOutlineArrowForwardIos } from 'react-icons/md';
 import img1 from '../../assets/how_it_works_img1.png';
 import img2 from '../../assets/how_it_works_img2.png';
 import img3 from '../../assets/how_it_works_img3.png';
+import DeliverModal from './../Modal/DeliverModal';
 
 const NavBar = () => {
   const navRef = useRef();
@@ -24,56 +25,6 @@ const NavBar = () => {
   //Show Modal
   const [showModal, setShowModal] = useState(false);
 
-  //Carousel
-  const slides = [
-    {
-      text: {
-        heading: 'Create an account',
-        span: 'by clicking on deliver votes',
-        body: 'Create an account in a few easy steps by using your favourite social media account.',
-      },
-      image: img1,
-    },
-    {
-      text: {
-        heading: 'Provide your',
-        span: 'details',
-        body: 'Provide your details in few simple steps, all you have to do is login with your social media account.',
-      },
-      image: img2,
-    },
-    {
-      text: {
-        heading: 'Answer a',
-        span: 'few questions',
-        body: 'Provide a few answers about your PVC status in order to get the neccessary information needed.',
-      },
-      image: img3,
-    },
-  ];
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  const slideInterval = useRef();
-
-  const prev = () => {
-    // startSlideTimer();
-
-    const index = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
-    setCurrentSlide(index);
-  };
-
-  const next = () => {
-    // startSlideTimer();
-
-    const index = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
-    setCurrentSlide(index);
-  };
-
-  const switchIndex = (index) => {
-    // startSlideTimer();
-    setCurrentSlide(index);
-  };
   //Router
   const router = useRouter();
 
@@ -138,116 +89,14 @@ const NavBar = () => {
             Deliver Votes
           </button>
           {/* </Link> */}
-          {/* CONTRIBUTE MODAL */}
+          {/* DELIVER VOTES MODAL */}
           {showModal && (
-            <Modal
+            <DeliverModal
               show={showModal}
               onClose={() => setShowModal(false)}
-              width="79.4rem"
-            >
-              <div className={styles.modal}>
-                <div className={`${styles.modal__heading} modal_heading`}>
-                  <h2>
-                    Learn more <br />
-                    <span>About the campaign</span>
-                  </h2>
-                  <button
-                    className={`closeBtn`}
-                    onClick={() => setShowModal(false)}
-                  >
-                    &times;
-                  </button>
-                </div>
-                <div className={styles.modal__body}>
-                  <div className={styles.carousel}>
-                    <div
-                      className={styles.carousel__inner}
-                      style={{
-                        transform: `translate(${-currentSlide * 100}%)`,
-                      }}
-                    >
-                      {slides.map((slide, index) => {
-                        return (
-                          <div className={styles.carousel__item}>
-                            <div className={styles.details}>
-                              <div className={styles.text}>
-                                <h3>
-                                  {slide.text.heading} <br />
-                                  <span>{slide.text.span}</span>
-                                </h3>
-                                <p>{slide.text.body}</p>
-                              </div>
-                              <div className={styles.img}>
-                                <Image src={slide.image} />
-                              </div>
-                              <div className={styles.text}>
-                                <h3>
-                                  {slides[index + 1]?.text.heading}
-                                  <br />
-                                  <span>{slides[index + 1]?.text.span}</span>
-                                </h3>
-                                <p>{slides[index + 1]?.text.body}</p>
-                              </div>
-                              <div className={styles.img}>
-                                <Image src={slides[1].image} />
-                              </div>
-                              {/* <div className={styles.text}>
-                                <h3>
-                                  {slides[2].textOne.heading}
-                                  <br />
-                                  <span>{slides[2].textOne.span}</span>
-                                </h3>
-                                <p>{slides[2].textOne.body}</p>
-                              </div>
-                              <div className={styles.img}>
-                                <Image src={slides[2].image} />
-                              </div> */}
-                            </div>
-                          </div>
-                        );
-                      })}
-                    </div>
-
-                    <div
-                      className={`${styles.carousel__control} ${
-                        currentSlide === 0 ? styles.slideNone : styles.left
-                      }`}
-                      onClick={prev}
-                    >
-                      <MdOutlineArrowBackIosNew />
-                    </div>
-                    <div
-                      className={`${styles.carousel__control} ${
-                        currentSlide === slides.length - 1
-                          ? styles.slideNone
-                          : styles.right
-                      }`}
-                      onClick={next}
-                    >
-                      <MdOutlineArrowForwardIos />
-                    </div>
-                  </div>
-                  <div className={styles.second_layer}>
-                    <div className={styles.carousel_indicators}>
-                      {slides.map((_, index) => (
-                        <button
-                          className={`${styles.carousel_indicator_item} ${
-                            currentSlide === index ? styles.active : ''
-                          }`}
-                          onClick={() => switchIndex(index)}
-                        ></button>
-                      ))}
-                    </div>
-                    <Link href="/how-it-works">Learn More</Link>
-                  </div>
-                  <button type="submit" className="btn_dark rounded-full">
-                    Create an account
-                  </button>
-                </div>
-              </div>
-            </Modal>
+            />
           )}
-          {/* END CONTRIBUTE MODAL */}
+          {/* END DELIVER VOTES MODAL */}
           <div
             onClick={showNavbar}
             className={`${styles.hamburger} ${styles.nav_open}`}
