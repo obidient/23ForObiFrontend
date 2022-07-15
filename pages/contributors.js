aecc697e1ac43fdb3a9023c64b61311ec1e26656
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-07-15 11:38:40 +0100

    ft--contributors updated

--- pages/contributors.js
+++ pages/contributors.js
@@ -4,12 +4,17 @@ import TeamCard from '../components/ImgCard/TeamCard';
 import Page from '../components/Page';
 import Team from '../data/teamImages';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css';
 
 const roles = ['Engineering', 'Design', 'Data Entry', 'Copywriting'];
+
 const pages = [1, 2, 3, 4, 5];
 
 const contributors = () => {
-  const [activeTab, setActiveTab] = useState('');
+  const [activeTab, setActiveTab] = useState(roles[0]);
+  const [tabIndex, setTabIndex] = useState(1);
+
   return (
     <Page title="Contributors" description="contributors for peter obi">
       <div className="container bg-[#FDFFFE]">
@@ -23,28 +28,51 @@ const contributors = () => {
         <div className="flex flex-col my-15">
           <h2 className="font-bold text-3xl">Meet our team members</h2>
         </div>
-        <div className="flex flex-col w-full my-6">
-          <div className="flex w-full my-10">
-            <ul className="flex border-b border-[#979797] w-full items-center justify-start text-center">
-              {roles.map((role) => (
-                <li
-                  className={
-                    activeTab
-                      ? 'border-[#018226] text-[#018226]'
-                      : 'font-bold px-8 py-3 text-2xl border-b-2  w-[200px] cursor-pointer hover:border-[#018226] hover:text-[#018226]'
-                  }
-                  key={role}
+        <div className="flex flex-col w-full my-10">
+          <Tabs
+            selectedIndex={tabIndex}
+            onSelect={(index) => setTabIndex(index)}
+            selectedTabClassName="border-b-2 border-[#018226] text-[#018226] outline-none"
+          >
+            <TabList className="flex border-b border-[#979797] w-full items-center justify-start text-center">
+              {roles.map((item) => (
+                <Tab
+                  key={item}
+                  className="font-bold px-8 py-3 text-3xl lg:text-2xl  w-[200px] cursor-pointer hover:border-[#018226] hover:text-[#018226] hover:border-b-2 "
                 >
-                  {role}
-                </li>
+                  {item}
+                </Tab>
               ))}
-            </ul>
-          </div>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-            {Team.map((item, index) => (
-              <TeamCard key={index} src={item.src} linkedIn={item.linkedIn} />
-            ))}
-          </div>
+            </TabList>
+            <TabPanel>
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+                {Team.map((item) => (
+                  <TeamCard src={item.src} />
+                ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+                {Team.map((item) => (
+                  <TeamCard src={item.src} />
+                ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+                {Team.map((item) => (
+                  <TeamCard src={item.src} />
+                ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+                {Team.map((item) => (
+                  <TeamCard src={item.src} />
+                ))}
+              </div>
+            </TabPanel>
+          </Tabs>
           <div className="flex w-full my-10">
             <div className="flex items-center justify-between border-[#C4C4C4] border-t-2 w-full">
               <p className="my-10 text-[#2F3733]">Showing 20 of 200 entries</p>
@@ -52,7 +80,7 @@ const contributors = () => {
                 <FaChevronLeft />
                 <ul className="flex justify-around items-center mx-4">
                   {pages.map((item) => (
-                    <div className="flex mx-3 items-center justify-center w-14 h-14 bg-[#F1F1F1] text-[#5F6160] font-light border-2 border-[#C4C4C4] w-10 h-10 text-center rounded-full hover:text-[#ffffff] hover:bg-[#018226]">
+                    <div className="flex mx-3 items-center justify-center w-14 h-14 bg-[#F1F1F1] text-[#5F6160] font-light border-2 border-[#C4C4C4] text-center rounded-full hover:text-[#ffffff] hover:bg-[#018226] cursor-pointer">
                       <li key={item} className="text-xl ">
                         {item}
                       </li>
