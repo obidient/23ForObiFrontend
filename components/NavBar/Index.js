a2fad0085533d3b0e33b98201d137af0e9de4ab2
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-07-25 11:10:43 +0100

    ft--blurry logo, line height and villages card updated

--- components/NavBar/Index.js
+++ components/NavBar/Index.js
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import Image from 'next/image';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/23forobi.svg';
 import styles from './Styles.module.scss';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { useRouter } from 'next/router';
@@ -40,7 +40,9 @@ const NavBar = () => {
         <nav className={styles.navbar}>
           <div className={styles.navbar__logo}>
             <Link href="/">
-              <Image src={logo} />
+              <>
+                <Image src={logo} width='130px' height='33px' />
+              </>
             </Link>
           </div>
           <div ref={navRef} className={styles.navbar__menu}>
