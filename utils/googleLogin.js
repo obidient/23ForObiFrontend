5ccedabc67b1513dda56673ef7f42f2b53f59497
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-07-27 08:34:30 +0100

    ft--updates for google client

--- utils/googleLogin.js
+++ utils/googleLogin.js
@@ -1,33 +1,38 @@
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import axios from 'axios';
+import {useRouter} from 'next/router'
 
 export default function GoogleAuth(props) {
+    const router = useRouter()
+  return (
+    <div>
+      <GoogleLogin
+        onSuccess={(res) => {
+          // console.log(res);
+          const url = 'https://api.23forobi.com/google/token';
+          const headers = {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          };
+          const data = {
+            token: res.credential,
+          };
 
-    return (
-
-        <div>
-            <GoogleLogin
-                onSuccess={(res) => {
-                    // console.log(res);
-                    const url = 'https://api.23forobi.com/google/token';
-                    const headers = {
-                        'Content-Type': 'application/json',
-                        Accept: 'application/json',
-                    };
-                    const data = {
-                        token: res.credential,
-                    };
-                    axios.post(url, data, headers).then((res) => {
-                        console.log(res);
-                    });
-                }}
-                onError={console.log('Login Failed')}  
-                text="signup_with"
-                size="large"
-                width="250%"
-                shape="circle"
-            />
-        </div>
-
-    );
+          axios
+            .post(url, data, headers)
+            .then((res) => {
+                console.log(res.data)
+                if(res.data && res.data.access_token){
+                    router.push('/dashboard')
+                }
+            });
+        }}
+        onError={console.log('Login Failed')}
+        text="signup_with"
+        size="large"
+        width="250%"
+        shape="circle"
+      />
+    </div>
+  );
 }
