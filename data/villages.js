94453d24d6e4f856ae566e799189f5c4907f7533
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-07-30 17:52:40 +0100

    ft--villages table

---
+++ data/villages.js
@@ -0,0 +1,32 @@
+const villages = [
+    {
+        id: 1,
+        name: 'Ukpor',
+        state: 'Anambra state',
+        contributors: 200,
+        progress: 100
+    },
+    {
+        id: 2,
+        name: 'Ejigbo',
+        state: 'Osun state',
+        contributors: 200,
+        progress: 20
+    },
+    {
+        id: 3,
+        name: 'Lugbe',
+        state: 'FCT Abuja',
+        contributors: 300,
+        progress: 30
+    },
+    {
+        id: 4,
+        name: 'Uwataku',
+        state: 'Borno State',
+        contributors: 90,
+        progress: 70
+    },
+]
+
+export default villages
