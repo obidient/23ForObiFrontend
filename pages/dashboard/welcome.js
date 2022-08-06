d4e7f4839fe6a95acf81bfaf99e28d656ab9890e
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-08-06 21:45:00 +0100

    ft--auth updated

--- pages/dashboard/welcome.js
+++ pages/dashboard/welcome.js
@@ -64,7 +64,7 @@ const welcome = () => {
   };
 
   return (
-    <div className={`container h-auto`}>
+    <div className={`container`}>
       <DashboardNav />
       <div className="flex flex-col justify-center py-11">
         <h2 className="text-4xl font-light mt-10">
