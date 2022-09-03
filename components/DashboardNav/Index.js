0cd45b5fc341f988e2fd06ba716cb58361f1a61b
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-09-03 14:55:55 +0100

    ft--scroll updates

--- components/DashboardNav/Index.js
+++ components/DashboardNav/Index.js
@@ -3,51 +3,13 @@ import styles from './Styles.module.scss';
 import logo from '../../assets/23forobi.svg';
 import caret_down from '../../assets/caret_down.png';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import DashboardLink from './DashboardLink';
 import Link from 'next/link';
 import StateProgress from './../misc/StateProgress';
-import useAuthStore from '../../store/authStore';
-import axios from 'axios';
 
-const DashboardNav = ({ progressbar, profile, voterData }) => {
-  const { accessToken } = useAuthStore();
-  const [votersProgress, setVotersProgress] = useState(0)
-  const [villaId, setVillaId] = useState(null)
-
-  useEffect(() => {
-    const fetchUserVilla = async () => {
-      await axios
-        .get('https://api.23forobi.com/user-villages', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        .then((res) => {
-          //console.log(res.data[0].village.id);
-          setVillaId(res.data[0].village.id)
-        });
-    };
-    fetchUserVilla()
-  }, [])
-
-  useEffect(() => {
-    const fetchVillaById = async () => {
-      await axios.get(`https://api.23forobi.com/voters/${villaId}`,{
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }).then((res) => {
-        console.log(res.data)
-        setVotersProgress(res.data.length)
-      })
-    }
-    fetchVillaById()
-  },[villaId])
-
-  const expectedVotes = 30;
-  const progress = Math.trunc((100 / expectedVotes) * votersProgress);
+const DashboardNav = ({ progress, progressbar, profile, voterData }) => {
   return (
     <div className={styles.dashboardnav}>
       <div className={styles.dashboardnav__top_left}>
@@ -64,7 +26,7 @@ const DashboardNav = ({ progressbar, profile, voterData }) => {
             </div>
             <StateProgress progress={progress} />
             <p className="text-[#979797] text-2xl">
-              (You have promised to deliver {votersProgress} votes)
+              (You have promised to deliver {voterData?.length} votes)
             </p>
           </div>
         )}
--- components/Welcome/Step5.js
+++ components/Welcome/Step5.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Step5 = ({ formData, setFormData }) => {
   const { full_name, phone } = formData;
   return (
-    <div className="my-5">
+    <div className="w-full overflow-auto">
       <h2 className="text-[#2F3733] lg:text-3xl my-10">
         Which voter from your village (apart from you) do you know who will
         surely vote for Peter Obi?
